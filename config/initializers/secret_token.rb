715784f18b74af23f642e64488fdf4b5b5cb94ec
vietnamusamail <vietnamusamail@gmail.com>
2012-06-09 15:47:52 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b20b3294f7306685aba717ce27c487114d22fa7af0cdedb481fd4101fee885665a15cf13484027511a47324cafd61586a19d8017fc3ec4faed70017a3a7b06a4'
